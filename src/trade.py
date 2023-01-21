089e1632bb700603b23470820a1594f371ad0d71
Srujith <srujith@kudikala.com>
2023-01-21 17:38:46 -0500

    fix a bunch of edges

--- src/trade.py
+++ src/trade.py
@@ -6,20 +6,21 @@ import math
 from decimal import Decimal
 import os
 
-purchase_allocations = json.loads(os.environ['purchase_allocations']) or {"BTC-USD":1.6,"ETH-USD":1.6}
-trade_usd_lower_limit = int(os.environ['trade_usd_lower_limit']) or 1
-trade_usd_upper_limit = int(os.environ['trade_usd_upper_limit']) or 50
-trade_interval_seconds = int(os.environ['trade_interval_seconds']) or  86400
-last_trade_timestamp = float(os.environ['last_trade_timestamp']) or 0
-last_trade_file = os.environ['last_trade_file'] or "/var/CoinbaseOp/last_trade_file"
-trade_offset_based_on_24h_percent_change = int(os.environ['trade_offset_based_on_24h_percent_change']) or 10
+purchase_allocations = json.loads(os.environ.get('purchase_allocations'))
+trade_usd_lower_limit = int(os.environ.get('trade_usd_lower_limit',1))
+trade_usd_upper_limit = int(os.environ.get('trade_usd_upper_limit', 50))
+trade_interval_seconds = int(os.environ.get('trade_interval_seconds',86400))
+last_trade_timestamp = float(os.environ.get('last_trade_timestamp',0))
+last_trade_file = os.environ.get('last_trade_file', "/var/CoinbaseOp/last_trade_file")
+trade_offset_based_on_24h_percent_change = int(os.environ.get('trade_offset_based_on_24h_percent_change', 10))
 conn = http.client.HTTPSConnection("api.coinbase.com")
-api_key = os.environ['api_key'] or "test"
-api_secret = os.environ['api_secret'] or "test"
+api_key = os.environ.get('api_key', "test")
+api_secret = os.environ.get('api_secret', "test")
 
 def canTrade():
     if time.time() - last_trade_timestamp < trade_interval_seconds :
-        print(f'Will skip all trading till {time.strftime("%A, %D %B %Y, %r, %nMOTY:%m %nDOTY:% j",time.localtime(last_trade_timestamp+trade_interval_seconds))}')
+        trade_paused_till =time.strftime("%A, %D %B %Y, %r",time.localtime(last_trade_timestamp+trade_interval_seconds))
+        print(f'Will skip all trading till {trade_paused_till}')
         return False
     else:
         return True
@@ -28,8 +29,8 @@ def sendRequest(method, apiEndpoint, payload, headers):
     global api_secret, api_key, conn
     timestamp = str(int(time.time()))
     message = timestamp + method + apiEndpoint.split('?')[0] + str(payload or '')
-    signature = hmac.new(api_secret, message, hashlib.sha256).hexdigest()
-    headers = headers + {
+    signature = hmac.new(api_secret.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).hexdigest()
+    headers = headers | {
     'Content-Type': 'application/json',
     'CB-ACCESS-KEY': api_key,
     'CB-ACCESS-SIGN': signature,
@@ -38,7 +39,11 @@ def sendRequest(method, apiEndpoint, payload, headers):
     conn.request(method, apiEndpoint, payload, headers)
     res = conn.getresponse()
     data = res.read()
-    return json.loads(data.decode("utf-8"))
+    try:
+        return json.loads(data.decode("utf-8"))
+    except:
+        print(f"we got back bad data from the api {apiEndpoint} data is:")
+        print(data.decode("utf-8"))
 
 def getAvailableUSD():
     payload = ''
@@ -56,7 +61,7 @@ def getPrice(currency_pair):
     headers = {
     'Content-Type': 'application/json'
     }
-    response = sendRequest("GET",f"api/v3/brokerage/products/{currency_pair}",payload,headers)
+    response = sendRequest("GET",f"/api/v3/brokerage/products/{currency_pair}",payload,headers)
     return {"price":Decimal(response["price"]), "price_percent_change_24h": Decimal(response["price_percentage_change_24h"]),"step_quote":Decimal(response["quote_increment"]), "step_base":Decimal(response["base_increment"])}
 
 def placeTrades(trades_to_place):
@@ -65,22 +70,24 @@ def placeTrades(trades_to_place):
         currency_pair = trade["currency_pair"]
         dollars = trade["dollars"]
         price_details = getPrice(currency_pair)
-        offset_percent = price_details["price_percent_change_24h"]*Decimal(trade_offset_based_on_24h_percent_change/100)
+        offset_percent = max(price_details["price_percent_change_24h"]*Decimal(trade_offset_based_on_24h_percent_change/100), Decimal(0.1))
         if dollars > 0 :
             limit_price = price_details["price"] - (price_details["price"] * offset_percent/Decimal(100))
             decimal_precision_quote = round(math.log(price_details["step_quote"],10))*-1
             decimal_precision_base = round(math.log(price_details["step_base"],10))*-1
             clean_limit_price = round(limit_price,decimal_precision_quote)
             amount = round(dollars/clean_limit_price,decimal_precision_base)
-            order_json_string = json.dumps({"side":"BUY","client_order_id":str(uuid.uuid1()),"product_id":currency_pair,"order_configuration":{"limit_limit_gtd":{"base_size":str(amount),"limit_price":str(clean_limit_price),"post_only":"true"}}})
+            order_json_string = json.dumps({"side":"BUY","client_order_id":str(uuid.uuid1()),"product_id":currency_pair,"order_configuration":{"limit_limit_gtc":{"base_size":str(amount),"limit_price":str(clean_limit_price),"post_only":True}}})
             headers = {
             'Content-Type': 'application/json'
             }
             response = sendRequest("POST","/api/v3/brokerage/orders",order_json_string,headers)
-            trades_placed = True
+            print(response)
+            if "error" not in response and "success" in response and response["success"]:
+                trades_placed = True
     if trades_placed :
         f = open(last_trade_file, "w")
-        f.write(f"last_trade_timestamp=\"{str(time.time())}\"")
+        f.write(f"last_trade_timestamp={str(time.time())}")
         f.close()
 
 
